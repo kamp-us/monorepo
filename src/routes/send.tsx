6c900e1b5b825ad088be04068ffd7168d9297fc6
Can Sirin <cansirin12@gmail.com>
2022-04-20 00:46:29 -0700

    migrate send page to remix run

--- src/routes/send.tsx
+++ src/routes/send.tsx
@@ -1,63 +1,66 @@
 import { useState } from "react";
 import { CenteredContainer } from "~/ui-library/layout-components/CenteredContainer";
-import { API } from "aws-amplify";
+import { API, Auth } from "aws-amplify";
 import { createPost } from "~/graphql/mutations";
 import { CreatePostMutationVariables } from "~/API";
-import { useUserContext } from "~/features/auth/user-context";
+import { AuthUser, useUserContext } from "~/features/auth/user-context";
 import { Button } from "~/ui-library/layout-components/Button";
 import { Input } from "~/ui-library/Input";
 import { GappedBox } from "~/ui-library/GappedBox";
 import { Box } from "~/ui-library/layout-components/Box";
 import { Label } from "~/ui-library/Label";
 import { useNavigate } from "react-router-dom";
+import { ActionFunction } from "remix";
+import { createClient } from "~/graphql/apollo-client";
+import { gql } from "@apollo/client";
+import { fetchUser } from "~/features/auth/useFetchUser";
+import { redirect } from "@remix-run/node";
+import { Form } from "~/ui-library/Form";
 
 const initialState = { title: "", url: "" };
 
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const title = formData.get("title");
+  const url = formData.get("url");
+
+  const client = await createClient();
+
+  let user: AuthUser | null = null;
+  try {
+    user = await fetchUser(Auth);
+  } catch {
+    user = null;
+  }
+
+  await client.mutate({
+    mutation: gql(createPost),
+    variables: {
+      input: {
+        title,
+        url,
+        owner: user?.username,
+      },
+    },
+  });
+
+  return redirect("/");
+};
+
 export const Send = () => {
-  const [formState, setFormState] = useState(initialState);
-  const user = useUserContext();
-  const navigate = useNavigate();
-
-  const sendPost = async () => {
-    try {
-      if (!formState.url || !formState.title) return;
-
-      const post = { ...formState, owner: user?.username };
-
-      await API.graphql({
-        query: createPost,
-        authMode: "AMAZON_COGNITO_USER_POOLS",
-        variables: { input: post } as CreatePostMutationVariables,
-      });
-      navigate("/");
-    } catch (e) {
-      console.log("error creating post", e);
-    }
-  };
-
-  const setInput = (key: string, value: any) => {
-    setFormState({ ...formState, [key]: value });
-  };
-
   return (
     <CenteredContainer css={{ width: 400 }}>
-      <GappedBox css={{ flexDirection: "column", marginTop: 10 }}>
-        <Label htmlFor="title">Başlık</Label>
-        <Input
-          id="title"
-          onChange={(event) => setInput("title", event.target.value)}
-          value={formState.title}
-        />
-        <Label htmlFor="url">URL</Label>
-        <Input
-          id="url"
-          onChange={(event) => setInput("url", event.target.value)}
-          value={formState.url}
-        />
-        <Box>
-          <Button onClick={sendPost}>Gönder</Button>
-        </Box>
-      </GappedBox>
+      <Form method="post">
+        <GappedBox css={{ flexDirection: "column", marginTop: 10 }}>
+          <Label htmlFor="title">Başlık</Label>
+          <Input id="title" name="title" />
+          <Label htmlFor="url">URL</Label>
+          <Input id="url" name="url" />
+          <Box>
+            <Button type="submit">Gönder</Button>
+          </Box>
+        </GappedBox>
+      </Form>
     </CenteredContainer>
   );
 };
