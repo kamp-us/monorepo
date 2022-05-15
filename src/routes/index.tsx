2cf9b6edfeb8fbe2ba860bddd19ec6262e2b493a
Can Sirin <cansirin12@gmail.com>
2022-05-15 01:34:32 -0700

    change apollo to SSR API graphql

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,37 +1,23 @@
-import { ListPostsQuery, ListPostsQueryVariables, Post } from "~/API";
+import { ListPostsQuery, Post } from "~/API";
 import { listPosts } from "~/graphql/queries";
 import { CenteredContainer } from "~/ui-library/layout-components/CenteredContainer";
 import { PostList } from "~/features/post/PostList";
-import { gql } from "@apollo/client";
 import { LoaderFunction, useLoaderData } from "remix";
-import { createClient } from "~/graphql/apollo-client";
+import { json } from "@remix-run/node";
+import { SSRGQL } from "~/graphql/SSRGQL";
 
 type LoaderData = {
   data: ListPostsQuery;
 };
 
-export const loader: LoaderFunction = async () => {
-  const client = await createClient();
+export const loader: LoaderFunction = async ({ request }) => {
+  const data = await SSRGQL({ request, query: listPosts });
 
-  try {
-    const { data, error } = await client.query<
-      ListPostsQuery,
-      ListPostsQueryVariables
-    >({
-      query: gql(listPosts),
-    });
-
-    if (error) {
-      console.log("index route", error);
-      // throw error;
-      return { data: null, error };
-    }
-
-    return { data };
-  } catch (error) {
-    console.log("index route", error);
-    return { data: null, error };
+  if (!data) {
+    return json(null, { status: 500 });
   }
+
+  return { data };
 };
 
 export const Home = () => {
