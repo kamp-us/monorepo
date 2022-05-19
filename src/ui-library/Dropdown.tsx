2f3ab5c10d4756bb953d27de8e306b35cd2996cf
usirin <umutsirin1@gmail.com>
2022-05-19 01:47:44 -0700

    Add Dropdown
    Signed-off-by: usirin <umutsirin1@gmail.com>

---
+++ src/ui-library/Dropdown.tsx
@@ -0,0 +1,99 @@
+import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
+import { styled } from "~/stitches.config";
+
+const StyledContent = styled(DropdownMenuPrimitive.Content, {
+  minWidth: 180,
+  backgroundColor: "$gray2",
+  borderRadius: 6,
+  padding: 5,
+  boxShadow:
+    "0px 10px 38px -10px $colors$gray5, 0px 10px 20px -15px $colors$gray6",
+});
+
+const itemStyles = {
+  all: "unset",
+  fontSize: 13,
+  lineHeight: 1,
+  color: "$gray11",
+  borderRadius: 3,
+  display: "flex",
+  alignItems: "center",
+  height: 25,
+  padding: "0 5px",
+  position: "relative",
+  paddingLeft: 25,
+  userSelect: "none",
+
+  "&[data-disabled]": {
+    color: "$gray8",
+    pointerEvents: "none",
+  },
+
+  "&:focus": {
+    backgroundColor: "$amber9",
+    color: "$amber1",
+  },
+};
+
+const StyledItem = styled(DropdownMenuPrimitive.Item, { ...itemStyles });
+const StyledCheckboxItem = styled(DropdownMenuPrimitive.CheckboxItem, {
+  ...itemStyles,
+});
+const StyledRadioItem = styled(DropdownMenuPrimitive.RadioItem, {
+  ...itemStyles,
+});
+const StyledTriggerItem = styled(DropdownMenuPrimitive.TriggerItem, {
+  '&[data-state="open"]': {
+    backgroundColor: "$amber4",
+    color: "$amber11",
+  },
+  ...itemStyles,
+});
+
+const StyledLabel = styled(DropdownMenuPrimitive.Label, {
+  paddingLeft: 25,
+  fontSize: 12,
+  lineHeight: "25px",
+  color: "$gray11",
+});
+
+const StyledSeparator = styled(DropdownMenuPrimitive.Separator, {
+  height: 1,
+  backgroundColor: "$gray6",
+  margin: 5,
+});
+
+const StyledItemIndicator = styled(DropdownMenuPrimitive.ItemIndicator, {
+  position: "absolute",
+  left: 0,
+  width: 25,
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const StyledArrow = styled(DropdownMenuPrimitive.Arrow, {
+  fill: "$gray2",
+});
+
+// Exports
+export const DropdownMenu = DropdownMenuPrimitive.Root;
+export const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
+export const DropdownMenuContent = StyledContent;
+export const DropdownMenuItem = StyledItem;
+export const DropdownMenuCheckboxItem = StyledCheckboxItem;
+export const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup;
+export const DropdownMenuRadioItem = StyledRadioItem;
+export const DropdownMenuItemIndicator = StyledItemIndicator;
+export const DropdownMenuTriggerItem = StyledTriggerItem;
+export const DropdownMenuLabel = StyledLabel;
+export const DropdownMenuSeparator = StyledSeparator;
+export const DropdownMenuArrow = StyledArrow;
+
+export const RightSlot = styled("div", {
+  marginLeft: "auto",
+  paddingLeft: 20,
+  color: "$gray11",
+  ":focus > &": { color: "white" },
+  "[data-disabled] &": { color: "$gray8" },
+});
