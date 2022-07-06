ab2bd5447c59ba759644dfe9c74af30541f1bc26
usirin <umutsirin1@gmail.com>
2022-07-06 16:19:03 -0700

    Update index page to use local gql query

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,35 +1,36 @@
-import { ListPostsQuery, Post } from "~/API";
-import { listPosts } from "~/graphql/queries";
+import { Post } from "~/API";
 import { CenteredContainer } from "~/ui-library/layout-components/CenteredContainer";
 import { PostList } from "~/features/post/PostList";
 import { json, LoaderFunction, useLoaderData } from "remix";
 import { withSSR } from "~/features/utils/amplify/withSSR";
+import { indexPageQuery } from "./index-page-query.server";
 
 type LoaderData = {
-  data: ListPostsQuery;
+  data: Post[];
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
   const { graphql } = withSSR({ request });
-  const data = await graphql({ query: listPosts });
+  const data = await graphql({ query: indexPageQuery, variables: {} });
 
   if (!data) {
     return json(null, { status: 500 });
   }
 
-  return { data };
-};
-
-export const Home = () => {
-  const { data } = useLoaderData<LoaderData>();
   const posts = (data.listPosts?.items as Post[]) || [];
   const sortedPosts = [...posts].sort((a, b) =>
     a.createdAt < b.createdAt ? 1 : -1
   );
 
+  return { data: sortedPosts };
+};
+
+export const Home = () => {
+  const { data } = useLoaderData<LoaderData>();
+
   return (
     <CenteredContainer css={{ paddingTop: 20 }}>
-      <PostList posts={sortedPosts} />
+      <PostList posts={data} />
     </CenteredContainer>
   );
 };
