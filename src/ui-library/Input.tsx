ce6885f571e60b986c0ddb6d9c5b057b3d8d1fb7
usirin <umutsirin1@gmail.com>
2022-07-06 15:40:38 -0700

    Update Input & Textarea styles

--- src/ui-library/Input.tsx
+++ src/ui-library/Input.tsx
@@ -1,17 +1,140 @@
 import { styled } from "~/stitches.config";
 
 export const Input = styled("input", {
-  background: "$gray3",
-  color: "$gray12",
-  height: "auto",
-  border: "1px solid $gray6",
-  fontSize: "$3",
-  padding: 5,
-  "&:hover": {
-    backgroundColor: "$gray4",
+  // Reset
+  appearance: "none",
+  borderWidth: "0",
+  boxSizing: "border-box",
+  fontFamily: "inherit",
+  margin: "0",
+  outline: "none",
+  padding: "0",
+  width: "100%",
+  WebkitTapHighlightColor: "rgba(0,0,0,0)",
+  "&::before": {
+    boxSizing: "border-box",
   },
+  "&::after": {
+    boxSizing: "border-box",
+  },
+
+  // Custom
+  backgroundColor: "$loContrast",
+  boxShadow: "inset 0 0 0 1px $colors$gray7",
+  color: "$hiContrast",
+  fontVariantNumeric: "tabular-nums",
+
+  "&:-webkit-autofill": {
+    boxShadow:
+      "inset 0 0 0 1px $colors$amber6, inset 0 0 0 100px $colors$amber3",
+  },
+
+  "&:-webkit-autofill::first-line": {
+    fontFamily: "$inter",
+    color: "$hiContrast",
+  },
+
   "&:focus": {
-    outline: "2px solid $amber6",
-    backgroundColor: "$gray5",
+    boxShadow:
+      "inset 0px 0px 0px 1px $colors$amber8, 0px 0px 0px 1px $colors$amber8",
+    "&:-webkit-autofill": {
+      boxShadow:
+        "inset 0px 0px 0px 1px $colors$amber8, 0px 0px 0px 1px $colors$amber8, inset 0 0 0 100px $colors$amber3",
+    },
+  },
+  "&::placeholder": {
+    color: "$gray9",
+  },
+  "&:disabled": {
+    pointerEvents: "none",
+    backgroundColor: "$gray2",
+    color: "$gray8",
+    cursor: "not-allowed",
+    "&::placeholder": {
+      color: "$gray7",
+    },
+  },
+  "&:read-only": {
+    backgroundColor: "$gray2",
+    "&:focus": {
+      boxShadow: "inset 0px 0px 0px 1px $colors$gray7",
+    },
+  },
+
+  variants: {
+    size: {
+      "1": {
+        borderRadius: "$1",
+        height: "$5",
+        fontSize: "$1",
+        px: "$1",
+        lineHeight: "$sizes$5",
+        "&:-webkit-autofill::first-line": {
+          fontSize: "$1",
+        },
+      },
+      "2": {
+        borderRadius: "$2",
+        height: "$6",
+        fontSize: "$3",
+        px: "$2",
+        lineHeight: "$sizes$6",
+        "&:-webkit-autofill::first-line": {
+          fontSize: "$3",
+        },
+      },
+    },
+    variant: {
+      ghost: {
+        boxShadow: "none",
+        backgroundColor: "transparent",
+        "@hover": {
+          "&:hover": {
+            boxShadow: "inset 0 0 0 1px $colors$grayA7",
+          },
+        },
+        "&:focus": {
+          backgroundColor: "$loContrast",
+          boxShadow:
+            "inset 0px 0px 0px 1px $colors$amber8, 0px 0px 0px 1px $colors$amber8",
+        },
+        "&:disabled": {
+          backgroundColor: "transparent",
+        },
+        "&:read-only": {
+          backgroundColor: "transparent",
+        },
+      },
+    },
+    state: {
+      invalid: {
+        boxShadow: "inset 0 0 0 1px $colors$red7",
+        "&:focus": {
+          boxShadow:
+            "inset 0px 0px 0px 1px $colors$red8, 0px 0px 0px 1px $colors$red8",
+        },
+      },
+      valid: {
+        boxShadow: "inset 0 0 0 1px $colors$green7",
+        "&:focus": {
+          boxShadow:
+            "inset 0px 0px 0px 1px $colors$green8, 0px 0px 0px 1px $colors$green8",
+        },
+      },
+    },
+    cursor: {
+      default: {
+        cursor: "default",
+        "&:focus": {
+          cursor: "text",
+        },
+      },
+      text: {
+        cursor: "text",
+      },
+    },
+  },
+  defaultVariants: {
+    size: "1",
   },
 });
