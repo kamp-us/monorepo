82e5ada05e19b41e2d9c940be5f7ad22a62deee4
Can Sirin <cansirin12@gmail.com>
2022-09-23 18:03:17 -0700

    User register (#70)
    * add register api route
    
    * configure docker files for dev env

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -69,3 +69,10 @@ export function useUser(): User {
 export function validateEmail(email: unknown): email is string {
   return typeof email === "string" && email.length > 3 && email.includes("@");
 }
+
+export function validateUsername(username: unknown): username is string {
+  return typeof username === "string" && username.length >= 2;
+}
+export function validatePassword(password: unknown): password is string {
+  return typeof password === "string" && password.length > 8;
+}
