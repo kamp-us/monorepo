d4baa9e747bbe7cb9695c0716e92cc164103f43d
Emir Boyacı <emirboyaci@icloud.com>
2022-10-02 01:01:58 +0300

    Introduce Standardize Formatting (#81)
    * feat: introduce standardize formatting
    
    Add husky and lint-staged dependencies, configure lint staged and husky pre-commit
    
    Refs: #80
    
    * chore: format all files
    
    Refs: #80

--- src/ui-library/StyleProvider.tsx
+++ src/ui-library/StyleProvider.tsx
@@ -1,4 +1,4 @@
-import type { FC} from "react";
+import type { FC } from "react";
 import { createContext, useCallback, useContext, useState } from "react";
 import { getCssText } from "~/stitches.config";
 
