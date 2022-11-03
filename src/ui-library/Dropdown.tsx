cd37ffa2ef8d2d4f318f5cc64a3b73c88c7c6b0a
Can Sirin <cansirin12@gmail.com>
2022-11-03 14:50:50 -0700

    update dropdown api

--- src/ui-library/Dropdown.tsx
+++ src/ui-library/Dropdown.tsx
@@ -1,5 +1,7 @@
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
-import { styled } from "~/stitches.config";
+import { ChevronRightIcon } from "@radix-ui/react-icons";
+import { keyframes, styled } from "~/stitches.config";
+import { $ElementProps } from "~/utils";
 
 const StyledContent = styled(DropdownMenuPrimitive.Content, {
   minWidth: 180,
@@ -41,7 +43,7 @@ const StyledCheckboxItem = styled(DropdownMenuPrimitive.CheckboxItem, {
 const StyledRadioItem = styled(DropdownMenuPrimitive.RadioItem, {
   ...itemStyles,
 });
-const StyledTriggerItem = styled(DropdownMenuPrimitive.TriggerItem, {
+const StyledTriggerItem = styled(DropdownMenuPrimitive.SubTrigger, {
   '&[data-state="open"]': {
     backgroundColor: "$amber4",
     color: "$amber11",
@@ -75,6 +77,78 @@ const StyledArrow = styled(DropdownMenuPrimitive.Arrow, {
   fill: "$gray2",
 });
 
+const slideUpAndFade = keyframes({
+  "0%": { opacity: 0, transform: "translateY(2px)" },
+  "100%": { opacity: 1, transform: "translateY(0)" },
+});
+
+const slideRightAndFade = keyframes({
+  "0%": { opacity: 0, transform: "translateX(-2px)" },
+  "100%": { opacity: 1, transform: "translateX(0)" },
+});
+
+const slideDownAndFade = keyframes({
+  "0%": { opacity: 0, transform: "translateY(-2px)" },
+  "100%": { opacity: 1, transform: "translateY(0)" },
+});
+
+const slideLeftAndFade = keyframes({
+  "0%": { opacity: 0, transform: "translateX(2px)" },
+  "100%": { opacity: 1, transform: "translateX(0)" },
+});
+
+const contentStyles = {
+  minWidth: 180,
+  backgroundColor: "$gray3",
+  borderRadius: 6,
+  padding: 5,
+  boxShadow: "0px 4px 8px 0px $colors$blackA9, 0px 0px 4px 0px $colors$blackA9",
+  color: "$amber11",
+
+  "@media (prefers-reduced-motion: no-preference)": {
+    animationDuration: "400ms",
+    animationTimingFunction: "cubic-bezier(0.16, 1, 0.3, 1)",
+    willChange: "transform, opacity",
+    '&[data-state="open"]': {
+      '&[data-side="top"]': { animationName: slideDownAndFade },
+      '&[data-side="right"]': { animationName: slideLeftAndFade },
+      '&[data-side="bottom"]': { animationName: slideUpAndFade },
+      '&[data-side="left"]': { animationName: slideRightAndFade },
+    },
+  },
+};
+
+const StyledSubContent = styled(DropdownMenuPrimitive.SubContent, {
+  ...contentStyles,
+});
+
+function SubContent(props: $ElementProps<typeof StyledSubContent>) {
+  return (
+    <DropdownMenuPrimitive.Portal>
+      <StyledSubContent {...props} />
+    </DropdownMenuPrimitive.Portal>
+  );
+}
+
+const StyledSubTrigger = styled(DropdownMenuPrimitive.SubTrigger, {
+  '&[data-state="open"]': {
+    backgroundColor: "$amber4",
+    color: "$amber11",
+  },
+  ...itemStyles,
+});
+
+export const MenuRightTrigger = ({ label }: { label: string }) => {
+  return (
+    <DropdownMenuSubTrigger>
+      {label}
+      <RightSlot>
+        <ChevronRightIcon />
+      </RightSlot>
+    </DropdownMenuSubTrigger>
+  );
+};
+
 // Exports
 export const DropdownMenu = DropdownMenuPrimitive.Root;
 export const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
@@ -88,11 +162,13 @@ export const DropdownMenuTriggerItem = StyledTriggerItem;
 export const DropdownMenuLabel = StyledLabel;
 export const DropdownMenuSeparator = StyledSeparator;
 export const DropdownMenuArrow = StyledArrow;
-
+export const DropdownMenuSubContent = SubContent;
+export const DropdownMenuSub = DropdownMenuPrimitive.Sub;
+export const DropdownMenuSubTrigger = StyledSubTrigger;
 export const RightSlot = styled("div", {
   marginLeft: "auto",
   paddingLeft: 20,
-  color: "$gray11",
-  ":focus > &": { color: "white" },
-  "[data-disabled] &": { color: "$gray8" },
+  color: "$amber11",
+  "[data-highlighted] > &": { color: "white" },
+  "[data-disabled] &": { color: "$amber8" },
 });
