8db6a6c128f78a021dcb3ee1e662a836b4d79305
Can Sirin <cansirin12@gmail.com>
2022-11-03 15:04:37 -0700

    add social media share buttons

--- src/ui-library/MoreOptionsDropdown.tsx
+++ src/ui-library/MoreOptionsDropdown.tsx
@@ -1,16 +1,26 @@
 import { DotsHorizontalIcon } from "@radix-ui/react-icons";
 import { useNavigate } from "@remix-run/react";
 import type { FC } from "react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import { canUserEdit } from "~/features/auth/can-user-edit";
 import { useUserContext } from "~/features/auth/user-context";
 import type { Post } from "~/models/post.server";
 import { styled } from "~/stitches.config";
+import { GappedBox } from "~/ui-library/GappedBox";
 import PostDeleteAlert from "~/ui-library/PostDeleteAlert";
+import {
+  FacebookShare,
+  RedditShare,
+  TwitterShare,
+} from "~/ui-library/SocialMediaButtons";
+import { Toast } from "~/ui-library/Toast";
+import { getExternalPostURL } from "~/utils";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./Dropdown";
 import { IconButton } from "./IconButton";
@@ -33,11 +43,17 @@ interface Props {
 
 export const MoreOptionsDropdown: FC<Props> = ({ post }) => {
   const user = useUserContext();
-  const [open, setOpen] = useState(false);
+  const [openAlert, setOpenAlert] = useState(false);
+  const [openToast, setOpenToast] = useState(false);
   const navigate = useNavigate();
+  const [externalPostUrl, setExternalPostUrl] = useState("");
+
+  useEffect(() => {
+    setExternalPostUrl(getExternalPostURL(post));
+  }, [post]);
 
   const handleOpen = () => {
-    setOpen(true);
+    setOpenAlert(true);
   };
 
   const ownerItems: JSX.Element[] = [];
@@ -68,10 +84,59 @@ export const MoreOptionsDropdown: FC<Props> = ({ post }) => {
             <DotsHorizontalIcon />
           </DotsButton>
         </DropdownMenuTrigger>
-
-        <DropdownMenuContent>{ownerItems}</DropdownMenuContent>
+        <DropdownMenuContent>
+          {ownerItems}
+          <DropdownMenuSeparator />
+          <MoreOptionsShareButtons
+            postUrl={externalPostUrl}
+            openToast={setOpenToast}
+          />
+        </DropdownMenuContent>
       </DropdownMenu>
-      <PostDeleteAlert open={open} setOpen={setOpen} postID={post.id} />
+      <PostDeleteAlert
+        open={openAlert}
+        setOpen={setOpenAlert}
+        postID={post.id}
+      />
+      <Toast
+        open={openToast}
+        setOpen={setOpenToast}
+        duration={5000}
+        title="Link kopyalandı"
+      />
+    </>
+  );
+};
+
+const SocialMediaShareButtons = ({ postUrl }: { postUrl: string }) => {
+  return (
+    <GappedBox css={{ paddingLeft: 25, paddingTop: 5 }}>
+      <FacebookShare size={20} postUrl={postUrl} />
+      <TwitterShare size={20} postUrl={postUrl} />
+      <RedditShare size={20} postUrl={postUrl} />
+    </GappedBox>
+  );
+};
+
+type MoreOptionsShareButtonsProps = {
+  postUrl: string;
+  openToast: (value: boolean) => void;
+};
+
+const MoreOptionsShareButtons = ({
+  postUrl,
+  openToast,
+}: MoreOptionsShareButtonsProps) => {
+  const onCopySelect = () => {
+    openToast(true);
+  };
+
+  return (
+    <>
+      <CopyToClipboard text={postUrl}>
+        <Item onSelect={onCopySelect}>Adresi kopyala</Item>
+      </CopyToClipboard>
+      <SocialMediaShareButtons postUrl={postUrl} />
     </>
   );
 };
