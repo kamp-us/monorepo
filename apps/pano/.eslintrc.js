cfbf0f9dcdca75a12f49f3869963bc0432471b3b
usirin <umutsirin1@gmail.com>
2022-11-13 01:13:32 -0800

    Create @kampus/eslint-config package

--- apps/pano/.eslintrc.js
+++ apps/pano/.eslintrc.js
@@ -2,36 +2,6 @@
  * @type {import('@types/eslint').Linter.BaseConfig}
  */
 module.exports = {
-  extends: [
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-    "@remix-run/eslint-config/jest-testing-library",
-    "prettier",
-  ],
-  rules: {
-    "@typescript-eslint/consistent-type-imports": "off",
-    "sort-imports": [
-      "error",
-      { ignoreCase: true, ignoreDeclarationSort: true },
-    ],
-    "import/order": [
-      "error",
-      {
-        groups: [
-          ["external", "builtin"],
-          "internal",
-          ["parent", "sibling", "index"],
-        ],
-        alphabetize: { order: "asc" },
-      },
-    ],
-  },
-  // we're using vitest which has a very similar API to jest
-  // (so the linting plugins work nicely), but it we have to explicitly
-  // set the jest version.
-  settings: {
-    jest: {
-      version: 27,
-    },
-  },
+  extends: ["@kampus/eslint-config", "@kampus/eslint-config/remix"],
+  root: true,
 };
