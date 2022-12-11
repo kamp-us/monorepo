d316a7776bfab57f68d4a7728f659b00a542e754
Can Soylu <94180156+csoylu@users.noreply.github.com>
2022-12-11 22:35:32 +0100

    Dropdown visibility (#176)
    fixes #154
    Post item used to check if user was able to edit the post to show the
    dropdown. I removed it.
    A check for userCanEdit already exists in dropdown, fixed some UI.
    
    There's also a commit on prisma seed that can make multiple users.

--- apps/pano/prisma/seed.ts
+++ apps/pano/prisma/seed.ts
@@ -1,36 +1,53 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
+const users = [
+  { username: "admin", email: "admin@kamp.us", password: "123123" },
+  { username: "testuser", email: "user1a@kamp.us", password: "123123123" }
+]
+
+type User = { username: string; email: string; password: string };
+
 const prisma = new PrismaClient();
 
-async function seed() {
-  const email = "admin@kamp.us";
-  const username = "admin";
+async function seedAll(users: User[]) {
+  for (const user of users) {
+    const email: string =  user.email;
+    const username: string = user.username;
 
-  const hashedPassword = await bcrypt.hash("123123123", 10);
+    const hashedPassword = await bcrypt.hash(user.password, 10);
 
-  const user = await prisma.user.upsert({
-    where: {
-      email,
-    },
-    update: {},
-    create: {
-      username,
-      email,
-      password: {
-        create: {
-          hash: hashedPassword,
+    const prismaUser = await prisma.user.upsert({
+      where: {
+        username
+      },
+      update: {
+        username,
+        email,
+        password: {
+          update: {
+            hash: hashedPassword,
+          },
         },
       },
-    },
-  });
+      create: {
+        username,
+        email,
+        password: {
+          create: {
+            hash: hashedPassword,
+          },
+        },
+      },
+    });
+  }
 }
 
-seed()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+seedAll(users)
+.catch((e) => {
+  console.error(e);
+  process.exit(1);
+})
+.finally(async () => {
+  await prisma.$disconnect();
+});
