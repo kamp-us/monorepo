b3cbeeac92ea5225aa31cd13c3ef438bce935334
Can SOYLU <soylucan@hotmail.fr>
2022-12-11 11:06:27 +0100

    user seed, create multiple users

--- apps/pano/prisma/seed.ts
+++ apps/pano/prisma/seed.ts
@@ -1,36 +1,45 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
+const users = [
+  { username: "admin", email: "admin@kamp", password: "123123123" },
+  { username: "testuser", email: "user1@kamp", password: "123123123" }
+]
+
+type User = { username: string; email: string; password: string };
+
 const prisma = new PrismaClient();
 
-async function seed() {
-  const email = "admin@kamp.us";
-  const username = "admin";
+async function seedAll(users: User[]) {
+  for (const user of users) {
+    const email: string =  user.email;
+    const username: string = user.username;
 
-  const hashedPassword = await bcrypt.hash("123123123", 10);
+    const hashedPassword = await bcrypt.hash(user.password, 10);
 
-  const user = await prisma.user.upsert({
-    where: {
-      email,
-    },
-    update: {},
-    create: {
-      username,
-      email,
-      password: {
-        create: {
-          hash: hashedPassword,
+    const prismaUser = await prisma.user.upsert({
+      where: {
+        email,
+      },
+      update: {},
+      create: {
+        username,
+        email,
+        password: {
+          create: {
+            hash: hashedPassword,
+          },
         },
       },
-    },
-  });
+    });
+  }
 }
 
-seed()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+seedAll(users)
+.catch((e) => {
+  console.error(e);
+  process.exit(1);
+})
+.finally(async () => {
+  await prisma.$disconnect();
+});
