0ab3012def51c3d4a5a998f6267367bb34fffee4
usirin <umutsirin1@gmail.com>
2022-12-18 01:52:52 -0800

    Start using softdelet for comment model

--- apps/pano/src/db.server.ts
+++ apps/pano/src/db.server.ts
@@ -39,6 +39,31 @@ function getClient() {
       },
     },
   });
+
+  client.$use(async (params, next) => {
+    if (params.model === "Comment") {
+      if (params.action == "delete") {
+        // Delete queries
+        // Change action to an update
+        params.action = "update";
+        params.args["data"] = { deletedAt: new Date() };
+      }
+      if (params.action == "deleteMany") {
+        if (!params.args) {
+          params.args = {};
+        }
+        // Delete many queries
+        params.action = "updateMany";
+        if (params.args.data != undefined) {
+          params.args.data["deletedAt"] = new Date();
+        } else {
+          params.args["data"] = { deletedAt: new Date() };
+        }
+      }
+    }
+    return next(params);
+  });
+
   // connect eagerly
   client.$connect();
 
