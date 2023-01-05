3cb92d65d3c8ed35d394cc1242b20c40382a9afa
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-01-05 20:21:42 +0300

    Add Backender interface and use it in PanoAPIServer

---
+++ internal/backend/backend.go
@@ -0,0 +1,4 @@
+package backend
+
+type Backender interface {
+}
--- server/server.go
+++ server/server.go
@@ -1,7 +1,9 @@
 package server
 
-type PanoAPIServer struct{}
+import "github.com/durmusrasit/pano-api/internal/backend"
 
-func NewPanoAPIServer() *PanoAPIServer {
-	return &PanoAPIServer{}
+type PanoAPIServer struct{ backend backend.Backender }
+
+func NewPanoAPIServer(backend backend.Backender) *PanoAPIServer {
+	return &PanoAPIServer{backend: backend}
 }
