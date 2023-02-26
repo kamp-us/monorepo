4d968933062be7a7c29c9497e92665e6c9537a8e
andyanday33 <andyanday33@gmail.com>
2023-02-26 00:56:47 +0000

    refactor(@kampus-apps/pano): rename "Divider" component imports to "Separator"

--- apps/pano/src/routes/posts/$slug-$id.tsx
+++ apps/pano/src/routes/posts/$slug-$id.tsx
@@ -2,9 +2,9 @@ import {
   Box,
   Button,
   CenteredContainer,
-  Divider,
   Form,
   GappedBox,
+  Separator,
   Text,
   Textarea,
   ValidationMessage,
@@ -175,7 +175,7 @@ const SinglePost = () => {
   return (
     <CenteredContainer css={{ gap: 5, pt: 20 }}>
       <PostItem post={post} showContent />
-      <Divider css={{ mt: 10 }} />
+      <Separator css={{ mt: 10 }} />
       <Box css={{ mt: 10 }}>
         <Text size={5} css={{ fontWeight: 500, color: "$gray11" }}>
           Yorumlar
