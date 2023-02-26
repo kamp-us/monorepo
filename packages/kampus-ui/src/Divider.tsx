adf904bec69bcf0efaaeafa05a858c2075b490b1
andyanday33 <andyanday33@gmail.com>
2023-02-26 00:55:37 +0000

    refactor(kampus-ui): rename "Divider" component to "Separator"

--- packages/kampus-ui/src/Divider.tsx
+++
@@ -1,6 +0,0 @@
-import { styled } from "~/stitches.config";
-import * as Separator from "@radix-ui/react-separator";
-
-export const Divider = styled(Separator.Root, {
-  border: "1px solid $gray9",
-});
---
+++ packages/kampus-ui/src/Separator.tsx
@@ -0,0 +1,6 @@
+import { styled } from "~/stitches.config";
+import * as RadixSeparator from "@radix-ui/react-separator";
+
+export const Separator = styled(RadixSeparator.Root, {
+  border: "1px solid $gray9",
+});
