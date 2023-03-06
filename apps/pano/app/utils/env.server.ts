6cc226e5da75d69f1a02776f9df413598c9ea911
ikadgzl <73023004+ikadgzl@users.noreply.github.com>
2023-03-06 03:40:49 +0300

    refactor: replace envsafe with znv (#268)
    Fixes #264
    
    ---------
    
    Co-authored-by: Can Sirin <cansirin12@gmail.com>

--- apps/pano/app/utils/env.server.ts
+++ apps/pano/app/utils/env.server.ts
@@ -1,15 +1,15 @@
-import { envsafe, str, url } from "envsafe";
+import { parseEnv } from "znv";
+import { z } from "zod";
 
-export const env = envsafe({
-  NODE_ENV: str({
-    devDefault: "development",
-    choices: ["development", "test", "production"],
-  }),
-  DATABASE_URL: url({
-    devDefault:
-      "postgresql://pgtest:pgtest@postgres:5432/pgtest?schema=public&connect_timeout=300",
-  }),
-  SESSION_SECRET: str({
-    devDefault: "sessionsecret",
-  }),
+export const env = parseEnv(process.env, {
+  NODE_ENV: z
+    .enum(["development", "test", "production"])
+    .default("development"),
+  DATABASE_URL: z
+    .string()
+    .url()
+    .default(
+      "postgresql://pgtest:pgtest@postgres:5432/pgtest?schema=public&connect_timeout=300"
+    ),
+  SESSION_SECRET: z.string().default("sessionsecret"),
 });
