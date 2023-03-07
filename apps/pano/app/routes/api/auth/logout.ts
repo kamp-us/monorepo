017123ced87f8c0c39b37f96deb99527c22edf89
Rasit Durmus <12167194+durmusrasit@users.noreply.github.com>
2023-03-07 03:12:48 +0300

    fix: the login page cannot be reached because of previous cookies (#284)
    Fixes #281

--- apps/pano/app/routes/api/auth/logout.ts
+++ apps/pano/app/routes/api/auth/logout.ts
@@ -2,5 +2,5 @@ import type { ActionFunction } from "@remix-run/node";
 import { authenticator } from "~/authenticator.server";
 
 export const action: ActionFunction = async ({ request }) => {
-  await authenticator.logout(request, { redirectTo: "/" });
+  return await authenticator.logout(request, { redirectTo: "/" });
 };
