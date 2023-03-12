526e8512899b0fe38cf5a0f710d08ff9c9145f66
andyanday33 <andyanday33@gmail.com>
2023-03-12 00:23:02 +0000

    revert(@kampus-apps/pano): revert discord only api/auth routes

--- apps/pano/app/routes/api/auth/discord.tsx
+++
@@ -1,9 +0,0 @@
-import type { ActionArgs } from "@remix-run/node";
-import { authenticator } from "~/authenticator.server";
-
-export const action = async ({ request }: ActionArgs) => {
-  return await authenticator.authenticate("discord", request, {
-    successRedirect: "/login",
-    failureRedirect: "/login",
-  });
-};
