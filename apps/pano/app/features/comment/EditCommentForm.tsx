eb41e90a23ed6d8789868101ca75f3e00453d5b5
uzanenis <uzanenis@outlook.com>
2023-03-26 20:29:43 +0300

    refactor: comment edit zod validation

--- apps/pano/app/features/comment/EditCommentForm.tsx
+++ apps/pano/app/features/comment/EditCommentForm.tsx
@@ -1,4 +1,4 @@
-import { Button, GappedBox, Textarea } from "@kampus/ui";
+import { Button, GappedBox, Textarea, ValidationMessage } from "@kampus/ui";
 import { useFetcher } from "@remix-run/react";
 import type { FC } from "react";
 import { useEffect } from "react";
@@ -15,16 +15,21 @@ export const EditCommentForm: FC<EditCommentProps> = ({
 }) => {
   const [editedComment, setEditedComment] = useState(comment.content);
   const fetcher = useFetcher();
+  const error = fetcher.data?.error;
+  console.log(fetcher);
   const isCommenting =
     fetcher.state === "submitting" || fetcher.state === "loading";
+
   const variables = {
     commentID: comment.id,
-    commentContent: editedComment,
+    commentContent: editedComment.trim(),
   };
 
   useEffect(() => {
-    if (fetcher.type === "done") {
-      setEditOpen(false);
+    if (!fetcher.data?.error) {
+      if (fetcher.type === "done") {
+        setEditOpen(false);
+      }
     }
   }, [fetcher.type, setEditOpen]);
 
@@ -42,6 +47,12 @@ export const EditCommentForm: FC<EditCommentProps> = ({
             {isCommenting ? "Kaydediliyor..." : "Kaydet"}
           </Button>
           <Button onClick={() => setEditOpen(false)}>İptal</Button>
+          {error && (
+            <ValidationMessage
+              error={error.message}
+              isSubmitting={isCommenting}
+            />
+          )}
         </GappedBox>
       </GappedBox>
     </fetcher.Form>
--- apps/pano/app/routes/commentEdit.tsx
+++ apps/pano/app/routes/commentEdit.tsx
@@ -1,6 +1,34 @@
 import type { ActionFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import invariant from "tiny-invariant";
+import { z } from "zod";
 import { editComment } from "~/models/comment.server";
+
+const CommentSchema = z.object({
+  commentID: z.string(),
+  commentContent: z.string().min(1),
+});
+type ActionData = {
+  error: {
+    message: string;
+    id: string | null;
+  };
+};
+
+const errorMessage = (message: string, id: string) => {
+  return json<ActionData>(
+    {
+      error: {
+        message,
+        id,
+      },
+    },
+    {
+      status: 400,
+    }
+  );
+};
+type CommentFields = z.infer<typeof CommentSchema>;
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
@@ -8,10 +36,16 @@ export const action: ActionFunction = async ({ request }) => {
 
   invariant(jsonData, "Missing json data");
 
-  const { commentID, commentContent } = JSON.parse(jsonData);
+  const parsedFields = JSON.parse(jsonData) as CommentFields;
+  const result = CommentSchema.safeParse(parsedFields);
+
+  if (!result.success) {
+    return errorMessage("Yorum boş gönderilemez.", parsedFields.commentID);
+  }
 
   try {
-    await editComment(commentID, commentContent);
+    const fields = CommentSchema.parse(parsedFields);
+    await editComment(fields.commentID, fields.commentContent);
   } catch (e) {
     return {
       status: 500,
