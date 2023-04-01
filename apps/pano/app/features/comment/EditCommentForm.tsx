7d404c80f13aa04379a8a5f76d3a3a08b24f95f3
Samet <sametkayik@gmail.com>
2023-04-02 00:11:16 +0300

    fix: add validation to prevent empty comments

--- apps/pano/app/features/comment/EditCommentForm.tsx
+++ apps/pano/app/features/comment/EditCommentForm.tsx
@@ -1,9 +1,11 @@
-import { Button, GappedBox, Textarea } from "@kampus/ui";
+import { Button, GappedBox, Textarea, ValidationMessage } from "@kampus/ui";
 import { useFetcher } from "@remix-run/react";
 import type { FC } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import type { Comment } from "~/models/comment.server";
+import { validate } from "~/utils";
+
 type EditCommentProps = {
   comment: Comment;
   setEditOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -14,6 +16,7 @@ export const EditCommentForm: FC<EditCommentProps> = ({
   setEditOpen,
 }) => {
   const [editedComment, setEditedComment] = useState(comment.content);
+  const [error, setError] = useState("");
   const fetcher = useFetcher();
   const isCommenting =
     fetcher.state === "submitting" || fetcher.state === "loading";
@@ -28,20 +31,35 @@ export const EditCommentForm: FC<EditCommentProps> = ({
     }
   }, [fetcher.type, setEditOpen]);
 
+  const handleCommentChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const value = event.target.value;
+    if (validate(value)) {
+      setEditedComment(value);
+      setError("");
+    } else {
+      setError("Yorum boş gönderilemez.");
+    }
+  };
+
   return (
     <fetcher.Form method="post" action="/commentEdit">
       <GappedBox css={{ flexDirection: "column" }}>
         <Textarea
           name="comment"
           defaultValue={comment.content}
-          onChange={(event) => setEditedComment(event.target.value)}
+          onChange={handleCommentChange}
         />
         <input type="hidden" name="json" value={JSON.stringify(variables)} />
         <GappedBox>
-          <Button type="submit">
+          <Button type="submit" disabled={error ? true : false}>
             {isCommenting ? "Kaydediliyor..." : "Kaydet"}
           </Button>
           <Button onClick={() => setEditOpen(false)}>İptal</Button>
+          {error && (
+            <ValidationMessage error={error} isSubmitting={isCommenting} />
+          )}
         </GappedBox>
       </GappedBox>
     </fetcher.Form>
