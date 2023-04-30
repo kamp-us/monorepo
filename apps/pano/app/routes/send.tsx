454725ea46b75b901afa0e4553ab737dc36344bc
Can Sirin <cansirin12@gmail.com>
2023-04-29 23:35:07 -0700

    refactor(@kampus-apps): variable clean up

--- apps/pano/app/routes/send.tsx
+++ apps/pano/app/routes/send.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   CenteredContainer,
+  Form,
   GappedBox,
   Input,
   Label,
@@ -10,14 +11,15 @@ import {
 } from "@kampus/ui";
 import type { ActionFunction, LoaderArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { useActionData, useFetcher, useLoaderData, useTransition } from "@remix-run/react";
+import { useActionData, useFetcher, useTransition } from "@remix-run/react";
 import parser from "html-metadata-parser";
 import { useEffect, useRef } from "react";
 import { z } from "zod";
 import { requireUser } from "~/authenticator.server";
 import type { inferSafeParseErrors } from "~/features/zod/utils";
+import { validateURLWithZod } from "~/features/zod/utils";
 import { createPost } from "~/models/post.server";
-import { getPostLink, validateURL } from "~/utils";
+import { getPostLink } from "~/utils";
 
 const SendSchema = z
   .object({
@@ -68,11 +70,9 @@ const badRequest = (data: ActionData) => json(data, { status: 400 });
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
   const fields = Object.fromEntries(formData.entries()) as SendFields;
-
   const user = await requireUser(request);
 
   const result = SendSchema.safeParse(fields);
-
   if (!result.success) {
     return badRequest({
       fields,
@@ -92,17 +92,15 @@ export const action: ActionFunction = async ({ request }) => {
 
 const Send = () => {
   const transition = useTransition();
-  const fetcher = useFetcher();
-  const meta = fetcher.data?.meta;
-  const loaderData = useLoaderData<typeof loader>();
-  const actionData = fetcher.data as ActionData;
-  const { errors } = actionData ?? {};
-
-  const { fieldErrors, formErrors } = errors ?? {};
+  const fetcher = useFetcher<typeof loader>();
+  const { meta, url } = fetcher.data ?? {};
+  const actionData = useActionData<ActionData>();
+  const { fieldErrors, formErrors } = actionData?.errors ?? {};
   const titleRef = useRef<HTMLInputElement>(null);
+  const isSubmitting = transition.state === "submitting";
 
   const fetchMetaTags = (url: string) => {
-    if (validateURL(url)) {
+    if (validateURLWithZod(url)) {
       const formData = new FormData();
       formData.set("url", url);
       fetcher.submit(formData, { method: "post", action: "/api/parse-meta" });
@@ -115,68 +113,48 @@ const Send = () => {
     // value needs to be updated manually since default value
     // comes populated on server-side before the fetcher fetches
     // meta title therefore will not update.
-    if (loaderData.meta?.title && titleRef.current && meta?.title)
-      titleRef.current.value = meta?.title;
-  }, [meta?.title, loaderData.meta?.title]);
+    if (titleRef.current && meta?.title) titleRef.current.value = meta.title;
+  }, [meta?.title]);
 
   return (
     <CenteredContainer css={{ paddingTop: 20 }}>
-      <fetcher.Form method="post">
+      <Form method="post">
         <GappedBox css={{ flexDirection: "column" }}>
           <Label htmlFor="url">URL</Label>
           <Input
             id="url"
             name="url"
             size="2"
-            defaultValue={loaderData.url ?? ""}
+            defaultValue={url ?? ""}
             onPaste={(e) => {
               fetchMetaTags(e.clipboardData.getData("text"));
             }}
+            // FIXME: We shouldn't fetch meta tags again on blur if the url is not changed
             onBlur={(e) => {
               fetchMetaTags(e.target.value);
             }}
           />
           {fieldErrors?.url && (
-            <ValidationMessage
-              error={fieldErrors.url[0]}
-              isSubmitting={transition.state === "submitting"}
-            />
+            <ValidationMessage error={fieldErrors.url[0]} isSubmitting={isSubmitting} />
           )}
           <Label htmlFor="title">Başlık</Label>
-          <Input
-            ref={titleRef}
-            id="title"
-            name="title"
-            size="2"
-            defaultValue={loaderData.meta?.title ?? meta?.title}
-          />
+          <Input ref={titleRef} id="title" name="title" size="2" defaultValue={meta?.title} />
           {fieldErrors?.title && (
-            <ValidationMessage
-              error={fieldErrors.title[0]}
-              isSubmitting={transition.state === "submitting"}
-            />
+            <ValidationMessage error={fieldErrors.title[0]} isSubmitting={isSubmitting} />
           )}
           <Label htmlFor="content">İçerik</Label>
           <Textarea css={{ width: "auto", cursor: "text" }} name="content" rows={4} />
           {fieldErrors?.content && (
-            <ValidationMessage
-              error={fieldErrors.content[0]}
-              isSubmitting={transition.state === "submitting"}
-            />
+            <ValidationMessage error={fieldErrors.content[0]} isSubmitting={isSubmitting} />
           )}
           <Box>
             <Button size="2" type="submit" variant="green">
               {transition.submission ? "Gönderiliyor..." : "Gönder"}
             </Button>
           </Box>
-          {formErrors && (
-            <ValidationMessage
-              error={formErrors[0]}
-              isSubmitting={transition.state === "submitting"}
-            />
-          )}
+          {formErrors && <ValidationMessage error={formErrors[0]} isSubmitting={isSubmitting} />}
         </GappedBox>
-      </fetcher.Form>
+      </Form>
     </CenteredContainer>
   );
 };
