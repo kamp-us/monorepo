6694826faced1ec4b93e64678bda49afc5ae02de
Can Sirin <cansirin12@gmail.com>
2023-04-29 23:34:21 -0700

    feat(@kampus-apps): add url validation with zod

--- apps/pano/app/features/zod/utils.ts
+++ apps/pano/app/features/zod/utils.ts
@@ -1,8 +1,13 @@
-import type { z } from "zod";
+import { z } from "zod";
 
 export type inferSafeParseErrors<T extends z.ZodType<any, any, any>, U = string> = {
   formErrors: U[];
   fieldErrors: {
     [P in keyof z.infer<T>]?: U[];
   };
+};
+
+export const validateURLWithZod = (url: string) => {
+  const result = z.string().url().safeParse(url);
+  return result.success;
 };
