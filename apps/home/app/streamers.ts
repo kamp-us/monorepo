81f093262404e7029c6e386182a20697da7221d5
sevilayerkan <sevilayerkan2@gmail.com>
2023-04-30 11:24:19 +0300

    Add more streamers streamers.ts

--- apps/home/app/streamers.ts
+++ apps/home/app/streamers.ts
@@ -3,6 +3,19 @@ export const streamers = [
   { username: "cansirin" },
   { username: "alicangunduzdev" },
   { username: "corefunctionsinitiated" },
-  { username: "geeksofdata" }
-  { username: "notdepressedeveloper" }
+  { username: "geeksofdata" },
+  { username: "notdepressedeveloper" },
+  { username: "wykonos" },
+  { username: "teachmejs" },
+  { username: "berankeday" },
+  { username: "harami" },
+  { username: "ozzy_efe" },
+  { username: "devshackle" },
+  { username: "mehmetcantasdev" },
+  { username: "iatee" },
+  { username: "thebeastofthemiddleeast" },
+  { username: "devketchuptv" },
+  { username: "creosb" },
+  { username: "berkaycodes" },
+
 ];
