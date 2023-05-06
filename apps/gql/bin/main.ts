45b0c0972063d3b3d1e28d41681dce7475f8cb30
usirin <umutsirin1@gmail.com>
2023-05-05 23:10:43 -0700

    feat(apps/gql): add dataloader & load users by using it

--- apps/gql/bin/main.ts
+++ apps/gql/bin/main.ts
@@ -5,7 +5,8 @@ import { Resolvers } from "../src/schema/types.generated";
 import { join } from "node:path";
 import { NodeHttpRPC } from "twirp-ts";
 import { env } from "../env";
-import { GetUserResponse, UsersClientProtobuf } from "@kampus-protos/users";
+import { GetBatchUsersRequest, UsersClientProtobuf } from "@kampus-protos/users";
+import DataLoader from "dataloader";
 
 const typeDefs = readFileSync(join(__dirname, "../src/schema/schema.graphql"), "utf8").toString();
 
@@ -13,9 +14,41 @@ const clients = {
   users: new UsersClientProtobuf(NodeHttpRPC({ baseUrl: env.USERS_TWIRP_HOST })),
 };
 
-const resolvers: Resolvers = {
+type UserLoaderKeyIdentifier = "id" | "username" | "email";
+
+type UserLoaderKey = `${UserLoaderKeyIdentifier}_${string}`;
+
+const loaders = {
+  users: new DataLoader(async (keys: readonly UserLoaderKey[]) => {
+    const request: GetBatchUsersRequest = {
+      ids: [],
+      usernames: [],
+      emails: [],
+    };
+
+    keys.forEach((key) => {
+      const [identifier, value] = key.split("_") as [UserLoaderKeyIdentifier, string];
+      if (identifier === "id") {
+        request.ids.push(value);
+      } else if (identifier === "username") {
+        request.usernames.push(value);
+      }
+    });
+
+    const response = await clients.users.GetBatchUsers(request);
+
+    const users = response.users || [];
+
+    return users.map((user) => ({
+      id: user.id,
+      username: user.username,
+    }));
+  }),
+};
+
+const resolvers: Resolvers<{ loaders: typeof loaders }> = {
   Query: {
-    user: async (_, { input }) => {
+    user: async (_, { input }, context) => {
       if (!input) {
         throw new Error("input is required");
       }
@@ -24,27 +57,7 @@ const resolvers: Resolvers = {
         throw new Error("id or username is required");
       }
 
-      let response: GetUserResponse;
-      if (input.id) {
-        response = await clients.users.GetUser({
-          identifier: { oneofKind: "id", id: input.id },
-        });
-      } else if (input.username) {
-        response = await clients.users.GetUser({
-          identifier: { oneofKind: "username", username: input.username },
-        });
-      } else {
-        return null;
-      }
-
-      if (!response.user) {
-        return null;
-      }
-
-      return {
-        id: response.user.id,
-        username: response.user.username,
-      };
+      return context.loaders.users.load(input.id ? `id_${input.id}` : `username_${input.username}`);
     },
   },
 
@@ -61,6 +74,9 @@ function main() {
     schema: createSchema({ typeDefs, resolvers }),
     logging: "debug",
     graphiql: true,
+    context: async () => ({
+      loaders,
+    }),
   });
 
   app.use("/graphql", yoga);
