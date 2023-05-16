5b2ef9effb62a3748bf7ce6ecc2f69c58207cd78
usirin <umutsirin1@gmail.com>
2023-05-15 18:24:22 -0700

    refactor(@kampus/ui): switch to css for AlertDialog
    Signed-off-by: usirin <umutsirin1@gmail.com>

--- packages/kampus-ui/src/AlertDialog.tsx
+++ packages/kampus-ui/src/AlertDialog.tsx
@@ -1,73 +1,28 @@
-import { blackA, mauve } from "@radix-ui/colors";
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
-import { keyframes, styled } from "@stitches/react";
 import type { FC } from "react";
 
-const overlayShow = keyframes({
-  "0%": { opacity: 0 },
-  "100%": { opacity: 1 },
-});
-
-const contentShow = keyframes({
-  "0%": { opacity: 0, transform: "translate(-50%, -48%) scale(.96)" },
-  "100%": { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
-});
-
-const StyledOverlay = styled(AlertDialogPrimitive.Overlay, {
-  backgroundColor: blackA.blackA9,
-  position: "fixed",
-  inset: 0,
-  "@media (prefers-reduced-motion: no-preference)": {
-    animation: `${overlayShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
-  },
-});
-
-const StyledContent = styled(AlertDialogPrimitive.Content, {
-  backgroundColor: "white",
-  borderRadius: 6,
-  boxShadow: "hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px",
-  position: "fixed",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "90vw",
-  maxWidth: "500px",
-  maxHeight: "85vh",
-  padding: 25,
-  "@media (prefers-reduced-motion: no-preference)": {
-    animation: `${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
-  },
-  "&:focus": { outline: "none" },
-});
+import "./AlertDialog.css";
 
 const Content: FC = ({ children, ...props }) => {
   return (
     <AlertDialogPrimitive.Portal>
-      <StyledOverlay />
-      <StyledContent {...props}>{children}</StyledContent>
+      <AlertDialogPrimitive.Overlay className="AlertDialogOverlay" />
+      <AlertDialogPrimitive.Content className="AlertDialogContent" {...props}>
+        {children}
+      </AlertDialogPrimitive.Content>
     </AlertDialogPrimitive.Portal>
   );
 };
 
-const StyledTitle = styled(AlertDialogPrimitive.Title, {
-  margin: 0,
-  color: mauve.mauve12,
-  fontSize: 17,
-  fontWeight: 500,
-});
-
-const StyledDescription = styled(AlertDialogPrimitive.Description, {
-  marginBottom: 20,
-  color: mauve.mauve11,
-  fontSize: 15,
-  lineHeight: 1.5,
-});
+function x<TProps>(Component: FC<TProps>, className: string) {
+  return (props: TProps) => <Component className={className} {...props} />;
+}
 
 // Exports
 export const AlertDialogBase = AlertDialogPrimitive.Root;
 export const AlertDialogTrigger = AlertDialogPrimitive.Trigger;
 export const AlertDialogContent = Content;
-export const AlertDialogTitle = StyledTitle;
-export const AlertDialogDescription = StyledDescription;
+export const AlertDialogTitle = x(AlertDialogPrimitive.Title, "AlertDialogTitle");
+export const AlertDialogDescription = x(AlertDialogPrimitive.Description, "AlertDialogDescription");
 export const AlertDialogAction = AlertDialogPrimitive.Action;
 export const AlertDialogCancel = AlertDialogPrimitive.Cancel;
