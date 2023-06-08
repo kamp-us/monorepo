ebdd2c5e4d5ad43e251157efe429550e79c9d7e3
usirin <umutsirin1@gmail.com>
2023-06-08 16:45:50 -0700

    feat(@kampus/ui-next): update import paths
    ---------
    
    Co-authored-by: Mert <88425310+6fy@users.noreply.github.com>

--- packages/ui/components/alert-dialog.tsx
+++ packages/ui/components/alert-dialog.tsx
@@ -3,8 +3,8 @@
 import * as React from "react";
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
 
-import { cn } from "~/utils";
-import { buttonVariants } from "~/components/button";
+import { cn } from "@kampus/ui-next/utils";
+import { buttonVariants } from "@kampus/ui-next/components/button";
 
 const AlertDialog = AlertDialogPrimitive.Root;
 
