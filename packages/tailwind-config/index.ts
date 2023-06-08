5f9afedc1d78d4259ab03d7d24b351cef0fe9f34
usirin <umutsirin1@gmail.com>
2023-06-07 17:04:39 -0700

    feat(@kampus/kampus): initial commit
    update everything to work with nextjs & tailwind

---
+++ packages/tailwind-config/index.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
