6f9fe76ebd83cb479b032cc506abfad351204b68
Can Sirin <cansirin12@gmail.com>
2023-06-12 00:27:10 -0700

    feat(@ui-next): add input with button

---
+++ apps/kampus/app/components/page.tsx
@@ -0,0 +1,12 @@
+import { InputWithButton } from "@kampus/ui-next";
+
+const ComponentsPage = () => {
+  return (
+    <div>
+      <h1>Custom Components</h1>
+      <InputWithButton placeholder="Search" />
+    </div>
+  );
+};
+
+export default ComponentsPage;
