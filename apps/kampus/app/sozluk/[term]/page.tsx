ed2e244fb20feda0a193291a5a96414ad54ddef7
usirin <umutsirin1@gmail.com>
2023-06-12 15:51:08 -0700

    feat(@kampus/kampus): add sozluk.kamp.us/[term] for showing sozluk terms

---
+++ apps/kampus/app/sozluk/[term]/page.tsx
@@ -0,0 +1,43 @@
+import { env } from "~/env";
+
+export default async function SozlukTermPage({ params }: { params: { term: string } }) {
+  const {
+    sozluk: { term },
+  } = await fetcher(makeQuery(params.term));
+
+  return (
+    <>
+      <h2 className="text-3xl font-bold">{term.title}</h2>
+      <div dangerouslySetInnerHTML={{ __html: term.body.html }} />
+    </>
+  );
+}
+
+const makeQuery = (id: string) => {
+  return `
+{
+  sozluk {
+    term(input: {id: "${id}"}) {
+      id
+      title
+      tags
+      body {
+        raw
+        code
+        html
+      }
+    }
+  }
+}`;
+};
+
+const fetcher = (query: string) =>
+  fetch(env.GQL_URL, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ query }),
+  })
+    .then((res) => res.json())
+    .then((json) => json.data);
