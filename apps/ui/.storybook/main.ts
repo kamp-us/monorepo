8deea63936d85c5f256f4e3e639017214be4144e
Furkan Gezek <furkangezek2014@gmail.com>
2023-06-13 02:53:47 +0300

    wip

--- apps/ui/.storybook/main.ts
+++ apps/ui/.storybook/main.ts
@@ -1,20 +1,15 @@
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
-const config = {
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-styling",
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        postCss: true,
-      },
-    },
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: "@storybook/react-vite",
     options: {},
   },
   docs: {
---
+++ apps/ui/stories/Button.stories.ts
@@ -0,0 +1,43 @@
+import {Button} from "@kampus/ui-next/components/button";
+import type {Meta, StoryObj} from "@storybook/react";
+
+const meta = {
+    component: Button,
+    tags: ["autodocs"],
+    args: {
+        children: "Button text",
+    },
+    argTypes: {
+        variant: {
+            options: ["default", "destructive", "outline", "secondary", "ghost", "link"],
+            control: {type: 'select'},
+            defaultValue: "default"
+        },
+        size: {
+            options: ["default", "sm", "lg"],
+            control: {type: 'select'},
+            defaultValue: "sm"
+        }
+    }
+} as Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default = {
+    args: {
+        size: "default"
+    },
+} satisfies Story;
+
+export const Small = {
+    args: {
+        size: "sm"
+    },
+} satisfies Story;
+
+export const Large = {
+    args: {
+        size: "lg"
+    },
+} satisfies Story;
