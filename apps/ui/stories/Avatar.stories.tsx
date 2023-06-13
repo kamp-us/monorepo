86d9a36cbe605de767c66fc036048eb7ca866ce6
Furkan Gezek <furkangezek2014@gmail.com>
2023-06-13 03:05:26 +0300

    feat(apps/ui): add Avatar story

---
+++ apps/ui/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import {Avatar, AvatarFallback, AvatarImage} from "@kampus/ui-next/components/avatar";
+import type {Meta, StoryObj} from "@storybook/react";
+
+const meta = {
+    component: Avatar,
+} as Meta<typeof Avatar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default = {
+    args: {
+        children: <>
+            <AvatarImage src="https://github.com/kamp-us.png" alt="@kampus"/>
+            <AvatarFallback>K</AvatarFallback>
+        </>
+    }
+} satisfies Story;
+
