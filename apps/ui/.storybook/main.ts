bb6ce76b086b3fcaf5f8f4b3ea707afe90794f88
Mert <88425310+6fy@users.noreply.github.com>
2023-06-13 04:22:06 +0300

    feat(@kampus/apps/ui): storybook (#417)
    the problem is tailwind css is not being added properly. who want to
    complete can continue
    
    ---------
    
    Co-authored-by: Furkan Gezek <furkangezek2014@gmail.com>

---
+++ apps/ui/.storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-styling",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
