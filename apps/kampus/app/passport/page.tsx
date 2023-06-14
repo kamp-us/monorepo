63b8ffc68c998550b4a4dd888f3894587f7586b9
Furkan Gezek <furkangezek2014@gmail.com>
2023-06-14 22:08:26 +0300

    fix: eslint error

--- apps/kampus/app/passport/page.tsx
+++ apps/kampus/app/passport/page.tsx
@@ -1,5 +1,5 @@
-import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth/next";
 import { authOptions } from "~/auth-config";
 import DiscordSignInButton from "~/components/DiscordSignInButton";
 
