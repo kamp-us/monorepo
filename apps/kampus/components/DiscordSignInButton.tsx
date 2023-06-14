4d67dc42b4fe50673a9e94ca4115a5b20b32a698
Furkan Gezek <furkangezek2014@gmail.com>
2023-06-14 21:53:54 +0300

    fix: invalid callback url

--- apps/kampus/components/DiscordSignInButton.tsx
+++ apps/kampus/components/DiscordSignInButton.tsx
@@ -5,7 +5,7 @@ import { signIn } from "next-auth/react";
 
 const DiscordSignInButton = () => {
   const searchParams = useSearchParams();
-  const callbackUrl: any = searchParams?.get("callbackUrl");
+  const callbackUrl: string = searchParams?.get("callbackUrl") ?? "/";
 
   return (
     <button
