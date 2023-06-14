f2ecbd9f6c3f8f7d39d24ba2e97cb63a88809b7f
Furkan Gezek <furkangezek2014@gmail.com>
2023-06-14 22:07:40 +0300

    chore: rename env names and add to turbo config

--- apps/kampus/auth-config.ts
+++ apps/kampus/auth-config.ts
@@ -1,13 +1,14 @@
-import DiscordProvider from "next-auth/providers/discord";
 import type { NextAuthOptions } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
 import { env } from "~/env";
+
 export const authOptions: NextAuthOptions = {
-  secret: env.SECRET,
+  secret: env.NEXTAUTH_SECRET,
 
   providers: [
     DiscordProvider({
-      clientId: env.DISCORD_ID,
-      clientSecret: env.DISCORD_SECRET,
+      clientId: env.DISCORD_CLIENT_ID,
+      clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
   ],
   pages: {
--- apps/kampus/env.ts
+++ apps/kampus/env.ts
@@ -4,15 +4,15 @@ export const env = parseEnv(
   {
     NODE_ENV: process.env.NODE_ENV,
     GQL_URL: process.env.GQL_URL,
-    DISCORD_ID: process.env.DISCORD_ID,
-    DISCORD_SECRET: process.env.DISCORD_SECRET,
-    SECRET: process.env.SECRET,
+    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
   {
     NODE_ENV: z.enum(["development", "test", "production"]),
     GQL_URL: z.string().url().default("http://localhost:3001/graphql"),
-    DISCORD_ID: z.string().default(""),
-    DISCORD_SECRET: z.string().default(""),
-    SECRET: z.string().default(""),
+    DISCORD_CLIENT_ID: z.string().default(""),
+    DISCORD_CLIENT_SECRET: z.string().default(""),
+    NEXTAUTH_SECRET: z.string().default(""),
   }
 );
