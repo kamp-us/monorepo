ff1e4d3a489151ad402baef932f5b0a7e6290201
usirin <umutsirin1@gmail.com>
2023-06-16 17:16:56 -0700

    chore(@kampus/kampus): fix eslint error

--- apps/kampus/app/sozluk/[term]/page.tsx
+++ apps/kampus/app/sozluk/[term]/page.tsx
@@ -1,6 +1,6 @@
 import loadSerializableQuery from "@kampus/relay/load-serializable-query";
-import SozlukTermQueryNode, { type SozlukTermQuery } from "./__generated__/SozlukTermQuery.graphql";
 import { SozlukTermContainer } from "~/app/sozluk/[term]/SozlukTerm";
+import SozlukTermQueryNode, { type SozlukTermQuery } from "./__generated__/SozlukTermQuery.graphql";
 
 export default async function SozlukTermPage({ params }: { params: { term: string } }) {
   const preloadedQuery = await loadSerializableQuery<SozlukTermQuery>(SozlukTermQueryNode, {
