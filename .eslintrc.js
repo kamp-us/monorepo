4f94b301b704f5aedb1bbd5ed2bdfb9990586f28
usirin <umutsirin1@gmail.com>
2023-06-20 00:51:35 -0700

    ci: add config/* as workspaces and move eslint config there

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,16 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   root: true,
-  extends: ["@kampus/eslint-config/next"],
+  extends: ["@kampus/eslint-config"], // uses the config in `config/eslint`
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json", "./apps/*/tsconfig.json", "./packages/*/tsconfig.json"],
+  },
   settings: {
     next: {
-      rootDir: ["apps/kampus", "apps/gql"],
+      rootDir: ["apps/nextjs", "apps/gql"],
     },
   },
 };
