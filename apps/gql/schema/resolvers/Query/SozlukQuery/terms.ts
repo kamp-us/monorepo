a5d4a4a7e08035b4895c7d352822ef45bd9e916d
Can Sirin <cansirin12@gmail.com>
2023-06-20 01:29:35 -0700

    fix(@kampus/gql): remove comments

--- apps/gql/schema/resolvers/Query/SozlukQuery/terms.ts
+++ apps/gql/schema/resolvers/Query/SozlukQuery/terms.ts
@@ -1,9 +1,5 @@
 import { type SozlukQueryResolvers } from "~/schema/types.generated";
 
 export const terms: SozlukQueryResolvers["terms"] = (_, args, { loaders }) => {
-  // Fetch all the terms
-
   return loaders.sozluk.terms.load({ ...args });
-
-  // Apply cursors to edges
 };
