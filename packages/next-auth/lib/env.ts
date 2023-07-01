46ab5cbc9ca389549e8ead9519338cfbc9ac775f
Uğur Oruç <uoruc5@hotmail.com>
2023-07-01 17:55:12 +0200

    adjust: env variables

--- packages/next-auth/lib/env.ts
+++ packages/next-auth/lib/env.ts
@@ -1,21 +1,31 @@
 import { parseEnv, z } from "znv";
 
-export const env = parseEnv(process.env, {
-  // Provider Secrets
-  // EMAIL_SERVER: z.string().default(""),
-  // EMAIL_FROM: z.string(),
+export const env = parseEnv(
+  {
+    GITHUB_ID: process.env.GITHUB_ID,
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+    DISCORD_ID: process.env.DISCORD_ID,
+    DISCORD_SECRET: process.env.DISCORD_SECRET,
+    TWITCH_ID: process.env.TWITCH_ID,
+    TWITCH_SECRET: process.env.TWITCH_SECRET,
+  },
+  {
+    // Provider Secrets
+    // EMAIL_SERVER: z.string().default(""),
+    // EMAIL_FROM: z.string(),
 
-  GITHUB_ID: z.string(),
-  GITHUB_SECRET: z.string(),
+    GITHUB_ID: z.string(),
+    GITHUB_SECRET: z.string(),
 
-  DISCORD_ID: z.string(),
-  DISCORD_SECRET: z.string(),
+    DISCORD_ID: z.string(),
+    DISCORD_SECRET: z.string(),
 
-  TWITCH_ID: z.string(),
-  TWITCH_SECRET: z.string(),
+    TWITCH_ID: z.string(),
+    TWITCH_SECRET: z.string(),
 
-  SECRET: z
-    .string()
-    .default("something")
-    .describe("A secret for signing the tokens and everything on next auth"),
-});
+    SECRET: z
+      .string()
+      .default("something")
+      .describe("A secret for signing the tokens and everything on next auth"),
+  }
+);
