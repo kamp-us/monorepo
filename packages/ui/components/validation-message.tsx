b486df1e41836f3e1701078af0433939c63b6fb8
cansirin <cansirin12@gmail.com>
2023-07-05 22:47:11 -0700

    add validationMessage

---
+++ packages/ui/components/validation-message.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState, type FC } from "react";
+
+type ValidationMessageProps = {
+  error: string;
+  isSubmitting?: boolean;
+};
+
+export const ValidationMessage: FC<ValidationMessageProps> = ({ error, isSubmitting = false }) => {
+  const [show, setShow] = useState(!!error);
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      const hasError = !!error;
+      setShow(hasError && !isSubmitting);
+    });
+    return () => clearTimeout(id);
+  }, [error, isSubmitting]);
+
+  return (
+    <div
+      className={`opacity-${show ? 100 : 0} text-amber-600 transition-all duration-500 ease-in-out`}
+    >
+      {error}
+    </div>
+  );
+};
