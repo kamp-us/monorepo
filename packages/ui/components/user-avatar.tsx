613a6b52a337a4b6437b250ddd54fa69c2d8dedc
cansirin <cansirin12@gmail.com>
2023-07-05 22:56:00 -0700

    add userAvatar

---
+++ packages/ui/components/user-avatar.tsx
@@ -0,0 +1,19 @@
+import React, { forwardRef } from "react";
+
+import { Avatar, AvatarFallback } from "./avatar";
+
+export interface UserAvatarProps {
+  login: string;
+}
+
+export const UserAvatar = forwardRef<HTMLSpanElement, UserAvatarProps>(({ login }, ref) => {
+  const firstLetter = login.charAt(0).toUpperCase();
+
+  return (
+    <Avatar>
+      <AvatarFallback ref={ref}>{firstLetter}</AvatarFallback>
+    </Avatar>
+  );
+});
+
+UserAvatar.displayName = "UserAvatar";
