30c378d702d7de2c6c5489cdccb2dd52ecb8192e
Furkan Gezek <furkangezek2014@gmail.com>
2023-07-09 20:27:16 +0300

    fix(pasaport): set auth cookies to root domain (#508)
    Closes #507 💯

--- packages/next-auth/lib/env.ts
+++ packages/next-auth/lib/env.ts
@@ -2,6 +2,7 @@ import { parseEnv, z } from "znv";
 
 export const env = parseEnv(
   {
+    AUTH_COOKIE_DOMAIN: process.env.AUTH_COOKIE_DOMAIN,
     GITHUB_ID: process.env.GITHUB_ID,
     GITHUB_SECRET: process.env.GITHUB_SECRET,
     DISCORD_ID: process.env.DISCORD_ID,
@@ -13,6 +14,8 @@ export const env = parseEnv(
     // Provider Secrets
     // EMAIL_SERVER: z.string().default(""),
     // EMAIL_FROM: z.string(),
+
+    AUTH_COOKIE_DOMAIN: z.string(),
 
     GITHUB_ID: z.string(),
     GITHUB_SECRET: z.string(),
