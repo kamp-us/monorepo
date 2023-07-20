cdc6cec919f47adde4787f837bd9b1ee02df55a8
usirin <umutsirin1@gmail.com>
2023-07-20 03:12:55 -0700

    feat(@kampus/gql-utils): add createPrismaConnectionLoader

---
+++ packages/gql-utils/prisma/create-connection-loader.ts
@@ -0,0 +1,39 @@
+import { findManyCursorConnection, type Connection } from "@devoxa/prisma-relay-cursor-connection";
+import DataLoader from "dataloader";
+
+import { type ConnectionKey } from "./connection-key";
+import { type PrismaModel } from "./types";
+
+export function createPrismaConnectionLoader<TPrisma extends { id: string }>(
+  table: PrismaModel<TPrisma>,
+  identifier: string | null,
+  onFetchComplete?: (connection: Connection<TPrisma>[]) => void
+) {
+  return new DataLoader(
+    async (keys: readonly ConnectionKey[]) => {
+      const items = await Promise.all(
+        keys.map(async (key) => {
+          if (identifier && !key.parentID) {
+            return null;
+          }
+
+          const where =
+            identifier && key.parentID
+              ? { [identifier]: key.parentID, deletedAt: null }
+              : { deletedAt: null };
+
+          return findManyCursorConnection(
+            (args) => table.findMany({ ...args, where }),
+            () => table.count({ where }),
+            key.arguments()
+          );
+        })
+      );
+
+      onFetchComplete?.(items.filter(Boolean) as Connection<TPrisma>[]);
+
+      return items;
+    },
+    { cacheKeyFn: (key) => key.hash() }
+  );
+}
