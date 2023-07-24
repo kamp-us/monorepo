74a011a17dad51cb5092b1e98f42a700437fe71c
Can Sirin <cansirin12@gmail.com>
2023-07-24 15:56:35 -0700

    refactor(@kampus-apps/pano): remove posts layout

--- apps/kampus/app/pano/posts/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function FiltersLayout({
-  children, // will be a page or nested layout
-}: {
-  children: React.ReactNode;
-}) {
-  return <section className="bg-amber-500">{children}</section>;
-}
