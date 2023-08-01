6bac060f3488419acdc80ff8468b90277a2e9c07
usirin <umutsirin1@gmail.com>
2023-07-31 17:03:16 -0700

    fix(apps/kampus): handle GQL_URL env variable correctly to work with client side better

--- apps/kampus/env.ts
+++ apps/kampus/env.ts
@@ -3,7 +3,7 @@ import { parseEnv, z } from "znv";
 export const env = parseEnv(
   {
     NODE_ENV: process.env.NODE_ENV,
-    GQL_URL: process.env.GQL_URL,
+    GQL_URL: process.env.NEXT_PUBLIC_GQL_URL,
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
