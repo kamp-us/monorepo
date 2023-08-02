499211aef98a5b7d897d29126dcc1c86e6ee5c2b
usirin <umutsirin1@gmail.com>
2023-08-01 19:38:01 -0700

    feat(apps/gql): expose pasaport session via gql context
    Closes #589

--- apps/gql/app/graphql/route.ts
+++ apps/gql/app/graphql/route.ts
@@ -1,21 +1,35 @@
 import { readFileSync } from "node:fs";
 import { join } from "node:path";
+import { type NextApiRequest, type NextApiResponse } from "next";
 import { createSchema, createYoga } from "graphql-yoga";
+
+import { getServerSession } from "@kampus/next-auth";
 
 import { createClients } from "~/clients";
 import { createLoaders } from "~/loaders";
 import { resolvers } from "~/schema/resolvers";
+import { type KampusGQLContext } from "~/schema/types";
 
 const typeDefs = readFileSync(join(process.cwd(), "schema/schema.graphql"), "utf8").toString();
 const clients = createClients();
 
-const { handleRequest } = createYoga({
-  schema: createSchema({ typeDefs, resolvers }),
+type ServerContext = { req: NextApiRequest; res: NextApiResponse } & KampusGQLContext;
+
+const { handleRequest } = createYoga<ServerContext>({
+  schema: createSchema<ServerContext>({ typeDefs, resolvers }),
   logging: "debug",
   graphiql: true,
-  context: () => ({
-    loaders: createLoaders(clients),
-  }),
+  context: async ({ req, res }) => {
+    const loaders = createLoaders(clients);
+    const session = await getServerSession(req, res);
+
+    return {
+      loaders,
+      pasaport: {
+        session,
+      },
+    } satisfies KampusGQLContext;
+  },
 
   fetchAPI: {
     Response,
--- apps/gql/schema/types.ts
+++ apps/gql/schema/types.ts
@@ -1,5 +1,10 @@
+import { type Session } from "@kampus/next-auth";
+
 import { type DataLoaders } from "~/loaders";
 
 export interface KampusGQLContext {
   loaders: DataLoaders;
+  pasaport: {
+    session: Session | null;
+  };
 }
