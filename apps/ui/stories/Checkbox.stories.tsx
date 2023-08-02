b97fb283ca30c68b62075cbbe488cc50632c5f2f
Murat Tunca <murattunca3@gmail.com>
2023-08-02 23:13:59 +0300

    feat(ui):add checkbox component and stories

---
+++ apps/ui/stories/Checkbox.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { Checkbox, Label } from "@kampus/ui-next";
+
+const labels = ["köpekleri çıkarmak", "alışverişe gitmek", "kitap okumak"];
+
+const meta = {
+  title: "Checkbox",
+  component: Checkbox,
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+type NewType = StoryObj<typeof meta>;
+
+export const Default = {
+  render: () => (
+    <div className="flex flex-col space-y-2 ">
+      <h4>Günlük Yapmam Gerekenler</h4>
+      {labels.map((label) => {
+        return (
+          <div className="flex items-center space-x-2">
+            <Checkbox id="terms" />
+            <Label
+              htmlFor="terms"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              {label}
+            </Label>
+          </div>
+        );
+      })}
+    </div>
+  ),
+};
