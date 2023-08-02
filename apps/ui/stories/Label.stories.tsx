5f2fb9f7cf93879ed9f268afbac0ef8fc4fbe0a5
Murat Tunca <murattunca3@gmail.com>
2023-08-02 23:24:37 +0300

    feat(apps/ui): add missing stories

---
+++ apps/ui/stories/Label.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { Checkbox, Label } from "@kampus/ui-next";
+
+const meta = {
+  title: "Label",
+  component: Label,
+} satisfies Meta<typeof Label>;
+
+export default meta;
+type NewType = StoryObj<typeof meta>;
+
+type Story = NewType;
+
+// export const Default = {} satisfies Story;
+export const Default = {
+  render: () => (
+    <div>
+      <div className="flex items-center space-x-2">
+        <Checkbox id="terms" />
+        <Label htmlFor="terms">Accept terms and conditions</Label>
+      </div>
+    </div>
+  ),
+};
