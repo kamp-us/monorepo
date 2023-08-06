0a8a98138b3fbd9048aa71f0d8d7a4106c09c229
usirin <umutsirin1@gmail.com>
2023-08-05 17:13:00 -0700

    feat(apps/kampus): initial commit with relay

---
+++ apps/kampus/app/pano/PostListContainer.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { graphql, usePreloadedQuery } from "react-relay";
+
+import { type SerializablePreloadedQuery } from "@kampus/relay";
+import useSerializablePreloadedQuery from "@kampus/relay/use-serializable-preloaded-query";
+
+import { type PostListContainerQuery } from "./__generated__/PostListContainerQuery.graphql";
+import { PanoFeed } from "./PanoFeed";
+
+interface Props {
+  preloadedQuery: SerializablePreloadedQuery<PostListContainerQuery>;
+}
+
+const query = graphql`
+  query PostListContainerQuery {
+    viewer {
+      ...PanoFeedFragment
+    }
+  }
+`;
+
+export const PostListContainer = (props: Props) => {
+  const queryRef = useSerializablePreloadedQuery(props.preloadedQuery);
+  const data = usePreloadedQuery(query, queryRef);
+
+  if (!data.viewer) {
+    return null;
+  }
+
+  return <PanoFeed panoFeed={data.viewer} />;
+};
