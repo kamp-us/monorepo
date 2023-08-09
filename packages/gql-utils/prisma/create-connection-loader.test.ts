d7142a2c9fd75b3bef37ceb79c1ccecf9cf38482
usirin <umutsirin1@gmail.com>
2023-08-09 12:54:24 -0700

    refactor(gql-utils): use mockPost instead of mockUser for connection loader tests

--- packages/gql-utils/prisma/create-connection-loader.test.ts
+++ packages/gql-utils/prisma/create-connection-loader.test.ts
@@ -1,58 +1,51 @@
 import { beforeEach, describe, expect, it, vi } from "vitest";
 
-import { mockedPrisma, mockUser } from "./__mocks__/prisma";
+import { mockedPrisma, mockPost, mockUser } from "./__mocks__/prisma";
 import { ConnectionKey } from "./connection-key";
 import { createPrismaConnectionLoader } from "./create-connection-loader";
 
 describe(createPrismaConnectionLoader, () => {
   beforeEach(() => {
-    mockedPrisma.user.findMany.mockResolvedValueOnce([
-      mockUser({ id: "1" }),
-      mockUser({ id: "2" }),
-      mockUser({ id: "3" }),
-      mockUser({ id: "4" }),
-      mockUser({ id: "5" }),
-      mockUser({ id: "6" }),
+    mockedPrisma.post.findMany.mockResolvedValueOnce([
+      mockPost({ id: "1" }, mockUser({ id: "1" })),
+      mockPost({ id: "2" }, mockUser({ id: "2" })),
+      mockPost({ id: "3" }, mockUser({ id: "3" })),
+      mockPost({ id: "4" }, mockUser({ id: "4" })),
+      mockPost({ id: "5" }, mockUser({ id: "5" })),
+      mockPost({ id: "6" }, mockUser({ id: "6" })),
     ]);
   });
 
   it("works without a foreign key", async () => {
-    const allUsers = createPrismaConnectionLoader(mockedPrisma.user, null);
+    const allUsers = createPrismaConnectionLoader(mockedPrisma.post, null);
 
     await allUsers.load(new ConnectionKey());
 
-    expect(mockedPrisma.user.findMany.mock.lastCall?.[0]?.where).toEqual({
+    expect(mockedPrisma.post.findMany.mock.lastCall?.[0]?.where).toEqual({
       deletedAt: null,
     });
   });
 
   it("returns null when there is an identifier but no parentID in key", () => {
-    // need to find a better example because "name" doesn't make sense with "parentID"
-    // "name" needs to be replaced with an "id" column to represent real usecases.
-    const allUsers = createPrismaConnectionLoader(mockedPrisma.user, "name");
+    const postsByUserID = createPrismaConnectionLoader(mockedPrisma.post, "userID");
 
     void expect(async () => {
-      await allUsers.load(new ConnectionKey());
-    }).rejects.toThrowError(/name/);
+      await postsByUserID.load(new ConnectionKey());
+    }).rejects.toThrowError(/userID/);
   });
 
   it("works", async () => {
-    // need to find a better example because "name" doesn't make sense with "parentID"
-    // "name" needs to be replaced with an "id" column to represent real usecases.
-    const allUsers = createPrismaConnectionLoader(mockedPrisma.user, "name");
+    const postsByUserID = createPrismaConnectionLoader(mockedPrisma.post, "userID");
+    const users = await postsByUserID.load(new ConnectionKey("umut"));
 
-    const users = await allUsers.load(new ConnectionKey("umut"));
     expect(users).not.toBeNull();
   });
 
   it("accepts a completion handler", async () => {
     const onComplete = vi.fn();
+    const postsByUserID = createPrismaConnectionLoader(mockedPrisma.post, "userID", onComplete);
 
-    // need to find a better example because "name" doesn't make sense with "parentID"
-    // "name" needs to be replaced with an "id" column to represent real usecases.
-    const allUsers = createPrismaConnectionLoader(mockedPrisma.user, "name", onComplete);
-
-    const users = await allUsers.load(new ConnectionKey("umut"));
+    const users = await postsByUserID.load(new ConnectionKey("umut"));
 
     expect(onComplete).toHaveBeenCalledWith([users]);
   });
