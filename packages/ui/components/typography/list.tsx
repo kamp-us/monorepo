269cfb6f2efc646785d2a18a5b8dea117b53a183
Murat Tunca <murattunca3@gmail.com>
2023-08-14 13:02:43 +0300

    fix(packages/ui) removed list items

--- packages/ui/components/typography/list.tsx
+++ packages/ui/components/typography/list.tsx
@@ -1,11 +1,5 @@
 import { PropsWithChildren } from "react";
 
 export function TypographyList(props: PropsWithChildren) {
-  return (
-    <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
-      <li>{props.children}</li>
-      <li>{props.children}</li>
-      <li>{props.children}</li>
-    </ul>
-  );
+  return <ul className="my-6 ml-6 list-disc [&>li]:mt-2">{props.children}</ul>;
 }
