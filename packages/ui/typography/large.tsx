18923d52cd35c0eddc58fa585c068dc6f81fdc99
Murat Tunca <murattunca3@gmail.com>
2023-08-14 11:16:54 +0300

    (@apps/ui): add to typographys

---
+++ packages/ui/typography/large.tsx
@@ -0,0 +1,3 @@
+export function TypographyLarge() {
+  return <div className="text-lg font-semibold">Are you sure absolutely sure?</div>;
+}
---
+++ packages/ui/typography/small.tsx
@@ -0,0 +1,3 @@
+export function TypographySmall() {
+  return <small className="text-sm font-medium leading-none">Email address</small>;
+}
