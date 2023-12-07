5a40654f0cb837ad442769dbc8ac1a15bece031e
Can Sirin <cansirin12@gmail.com>
2023-12-06 23:46:47 -0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   root: true,
-  extends: ['@kampus/eslint-config'], // uses the config in `config/eslint`
-  parser: '@typescript-eslint/parser',
-  ignorePatterns: ['**/*.generated.ts'],
+  extends: ["@kampus/eslint-config"], // uses the config in `config/eslint`
+  parser: "@typescript-eslint/parser",
+  ignorePatterns: ["**/*.generated.ts"],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: "latest",
     tsconfigRootDir: __dirname,
     project: [
-      './tsconfig.json',
-      './apps/*/tsconfig.json',
-      './db/*/tsconfig.json',
-      './packages/*/tsconfig.json',
+      "./tsconfig.json",
+      "./apps/*/tsconfig.json",
+      "./db/*/tsconfig.json",
+      "./packages/*/tsconfig.json",
     ],
   },
   settings: {
     next: {
-      rootDir: ['apps/kampus', 'apps/gql'],
+      rootDir: ["apps/kampus", "apps/gql"],
     },
   },
 }
