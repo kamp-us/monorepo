9e2e45db82042d8e4760adcbad7a8242aa4398ff
Can Sirin <cansirin12@gmail.com>
2023-12-08 00:43:53 -0800

    chore: remove unused files (#709)
    removing next auth
    removing deprecated folder
    bumping turbo version to v2

--- deprecated/home/app/features/logo/Logo.tsx
+++
@@ -1,9 +0,0 @@
-export const Logo = ({ height, width }: { height: number; width: number }) => {
-  return (
-    <img
-      src="https://kampus-logo.s3.eu-central-1.amazonaws.com/kampus_logo.png"
-      style={{ height: `${height}px`, width: `${width}px` }}
-      alt="logo"
-    />
-  );
-};
--- deprecated/kampus-ui/src/utils.tsx
+++
@@ -1,5 +0,0 @@
-export declare type $ElementProps<T> = T extends React.ComponentType<infer Props>
-  ? Props extends object
-    ? Props
-    : never
-  : never;
