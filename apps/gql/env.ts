22982c8a36c0b5da74f3c4d2884b32bb36d08022
Can Sirin <cansirin12@gmail.com>
2023-12-08 00:37:12 -0800

    remove next-auth

--- apps/gql/env.ts
+++ apps/gql/env.ts
@@ -5,7 +5,6 @@ export const env = parseEnv(
   {
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     KAMPUS_ENV: process.env.KAMPUS_ENV,
   },
   {
@@ -14,7 +13,6 @@ export const env = parseEnv(
       .string()
       .url()
       .default("mysql://kampus:kampus@localhost:3306/kampus?schema=public&connect_timeout=300"),
-    NEXTAUTH_URL: z.string().url().default("http://localhost:3001/auth"),
     KAMPUS_ENV: z.enum(["development", "test", "production"]).default("development"),
   }
 );
