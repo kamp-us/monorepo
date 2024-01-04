440116d74ddf3ac725b802cd66afc5a7fd884385
Huseyin Gulec <itshuseyingulec@gmail.com>
2024-01-04 15:37:09 +0000

    chore(title): add title icon and some design

--- apps/kampus/app/odin/temel-bilgiler-seruveni/[...lesson]/OdinLessonTitle.tsx
+++ apps/kampus/app/odin/temel-bilgiler-seruveni/[...lesson]/OdinLessonTitle.tsx
@@ -19,5 +19,16 @@ const useOdinLessonTitleFragment = (key: OdinLessonTitle_title$key | null) =>
 export const OdinLessonTitle = (props: Props) => {
   const lesson = useOdinLessonTitleFragment(props.lesson);
 
-  return <h2 className="text-3xl font-bold">{lesson?.title}</h2>;
+  return <header className="mb-10 mt-2">
+      <div className="flex flex-col items-center mb-2 space-y-4 xl:space-y-0 xl:space-x-4 xl:flex-row
+       xl:justify-start">
+      <a href="#" className="flex items-center justify-center w-24 h-24 bg-gray-200 rounded-full">
+        <img src="https://github.com/kamp-us/monorepo/assets/107408663/7317463f-fb13-403a-bf46-7db7fd734266" alt="Lesson Icon" />
+      </a>
+      <div className="text-center xl:text-left">
+          <h2 className="text-3xl font-bold">{lesson?.title}</h2>
+          <p className="text-lg font-light">Current Serüven</p>
+        </div>
+      </div>
+    </header>
 };
