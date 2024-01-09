3a7cd9975c209f1250d33c3a94eb74488722fbde
Huseyin Gulec <itshuseyingulec@gmail.com>
2024-01-09 20:58:26 +0000

    fix function name and broken links

--- apps/kampus/app/odin/temel-bilgiler-seruveni/[...lesson]/OdinLessonTitle.tsx
+++ apps/kampus/app/odin/temel-bilgiler-seruveni/[...lesson]/OdinLessonTitle.tsx
@@ -4,7 +4,7 @@ import { type OdinLessonTitle_title$key } from "./__generated__/OdinLessonTitle_
 
 import curriculumList, { Curriculum } from "../../curriculum-list";
 
-const getCurrentCurri = (curriculum: Curriculum[], currentTitle: string): { name: string, url: string } | null => {
+const getCurrentCurriculum = (curriculum: Curriculum[], currentTitle: string): { name: string, url: string } | null => {
   for (let curriculumItem of curriculum) {
     for (let section of curriculumItem.sections) {
       for (let lesson of section.lessons) {
@@ -40,21 +40,21 @@ export const OdinLessonTitle = (props: Props) => {
   let currentCurriculumUrl = null;
   let currentCurriculumName = null;
   if (currentTitle) {
-    const currentCurri = getCurrentCurri(curriculumList.curriculum, currentTitle);
-    currentCurriculumUrl = currentCurri?.url;
-    currentCurriculumName = currentCurri?.name;
+    const currentCurriculum = getCurrentCurriculum(curriculumList.curriculum, currentTitle);
+    currentCurriculumUrl = currentCurriculum?.url;
+    currentCurriculumName = currentCurriculum?.name;
   }
 
-
+console.log(lesson?.title)
   return <header className="mb-10 mt-2">
       <div className="flex flex-col items-center mb-2 space-y-4 xl:space-y-0 xl:space-x-4 xl:flex-row
        xl:justify-start">
-      <a href={currentCurriculumUrl || undefined} className="flex items-center justify-center w-24 h-24 bg-gray-200 rounded-full">
+      <a href={`/odin/${currentCurriculumUrl || undefined}`} className="flex items-center justify-center w-24 h-24 bg-gray-200 rounded-full">
         <img src="https://github.com/kamp-us/monorepo/assets/107408663/7317463f-fb13-403a-bf46-7db7fd734266" alt="Lesson Icon" />
       </a>
       <div className="text-center xl:text-left">
           <h2 className="text-3xl font-bold">{lesson?.title}</h2>
-          <a className="text-lg font-light" href={currentCurriculumUrl || undefined}>{currentCurriculumName}</a>
+          <a className="text-lg font-light" href={`/odin/${currentCurriculumUrl || undefined}`}>{currentCurriculumName}</a>
         </div>
       </div>
     </header>
